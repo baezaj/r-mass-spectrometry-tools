1769128a08cbc9bb6907680807b49acec39828fb
baezaj <baeza@pennmedicine.upenn.edu>
2023-12-21 12:14:15 -0500

    refactoring read_rawfiles.R

--- bin/read_rawfiles.R
+++ bin/read_rawfiles.R
@@ -1,21 +1,25 @@
 
 
-### Libraries ###
+# Libraries ---------------------------------------------------------------
+
 
 library(rawDiag)
 library(tidyverse)
-library(rio)
 library(lubridate)
 
 
-### Data import ###
 
 # Directory path
-raw_file_dir <- "../data/raw/"
+raw_file_dir <- "../path/to/raw/files"
+
+
+# Annotation File ---------------------------------------------------------
+
 
 # Reading the files in the directory
 raw_files <- list(filename = dir(raw_file_dir)[grepl(".raw", dir(raw_file_dir))],
-                  creation_date = file.info(dir(raw_file_dir, full.names = TRUE)[grepl(".raw", dir(raw_file_dir))])$mtime)
+                  creation_date = file.info(dir(raw_file_dir, full.names = TRUE)[grepl(".raw", dir(raw_file_dir))])$mtime,
+                  file_size = file.info(dir(raw_file_dir, full.names = TRUE)[grepl(".raw", dir(raw_file_dir))])$size)
 
 # converting the files into dataframe
 annot <- bind_rows(raw_files)
@@ -23,12 +27,11 @@ annot <- bind_rows(raw_files)
 # Formatting annot table
 annot <- annot %>%
   mutate(filename = str_remove_all(filename, ".raw"),
-         creation_date = ymd_hms(creation_date)) %>%
-  separate(filename, into = c("date", "user", "nls", "instrument", "ms_type", "condition_1",
-                              "time_point", "batch", "replicate", "techrep", "run_order"),
-           sep = "_", remove = FALSE, convert = TRUE)
+         creation_date = ymd_hms(creation_date))
 
-### Reading raw files using RawDiag ###
+
+# RawDiag -----------------------------------------------------------------
+
 
 # Reading list of raw files from the directory
 raw_file_names <- dir(raw_file_dir, full.names = TRUE)[grep(".raw$", list.files(raw_file_dir))]
@@ -39,12 +42,10 @@ RAW <- lapply(raw_file_names, read.raw)
 # Concatenating list into a single dataframe
 data <- bind_rows(RAW)
 
-### Formatting ###
 
 # cleanup of raw file names
 data <- data %>%
-  mutate(filename = str_remove_all(filename, ".raw"))
+  mutate(filename = str_remove_all(filename, ".raw")) %>% 
+  full_join(annot, .)
 
-# adding annotation to dataframe
-data <- full_join(annot, data)
 
